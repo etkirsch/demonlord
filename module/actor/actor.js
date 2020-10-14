639a0bacc1c2419be18124c99f0801d41af4f4c6
Evan Kirsch <etkirsch@users.noreply.github.com>
2020-10-14 10:05:24 -0400

    we're back here again.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -109,7 +109,7 @@ export class DemonlordActor extends Actor {
             data.characteristics.defense = parseInt(data.characteristics.defense) + parseInt(defenseBonus) + parseInt(agilitypoint);
 
         data.characteristics.defense = parseInt(data.characteristics.defense) + parseInt(characterbuffs.defensebonus);
-        // data.characteristics.power = parseInt(data.characteristics.power) + parseInt(characterbuffs.powerbonus);
+        data.characteristics.power = parseInt(data.characteristics.power) + parseInt(characterbuffs.powerbonus);
 
         characterbuffs.speedbonus += speedPenalty;
 
@@ -153,6 +153,7 @@ export class DemonlordActor extends Actor {
                 "data.characteristics.powerbonus": parseInt(characterbuffs.powerbonus) - (parseInt(child.data.bonuses.power) ? parseInt(child.data.bonuses.power) : 0),
                 "data.characteristics.speedbonus": parseInt(characterbuffs.speedbonus) - (parseInt(child.data.bonuses.speed) ? parseInt(child.data.bonuses.speed) : 0),
                 "data.characteristics.defense": parseInt(this.data.data.characteristics.defense) - (parseInt(child.data.bonuses.defense) ? parseInt(child.data.bonuses.defense) : 0),
+                "data.characteristics.power": parseInt(this.data.data.characteristics.power) - (parseInt(child.data.bonuses.power) ? parseInt(child.data.bonuses.power) : 0),
                 "data.characteristics.health.max": parseInt(this.data.data.characteristics.health.max) - (parseInt(child.data.bonuses.health) ? parseInt(child.data.bonuses.health) : 0),
                 "data.characteristics.speed.value": parseInt(this.data.data.characteristics.speed.value) - (parseInt(child.data.bonuses.speed) ? parseInt(child.data.bonuses.speed) : 0)
             });
